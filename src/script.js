1c6e70e6598cc5574229340dc3f0f20c85b7e9c0
Primabratinella <karen.pearl@hotmail.com>
2021-06-10 22:46:52 -0400

    added icon element attribute

--- src/script.js
+++ src/script.js
@@ -25,39 +25,51 @@ function formatDate(date) {
 
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
+  let searchCity = "Germantown";
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
-
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  axios.get(apiUrl).then(displayWeatherCondition);
+
+  iconElement.setAttribute (
+    "alt", response.data.weather[0].main
+  );
 }
 
 function searchCity(city) {
   let apiKey = "32560c27e8e23a3db17cfaff2b468cd2";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
+  
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
   searchCity(city);
+  
 }
 
 function searchLocation(position) {
   let apiKey = "32560c27e8e23a3db17cfaff2b468cd2";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
+  
 }
 
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
+  let currentLocationButton = document.querySelector("#current-location-button");
+  currentLocationButton.addEventListener("click", getCurrentLocation);
 }
 
 function convertToFahrenheit(event) {
@@ -78,11 +90,5 @@ dateElement.innerHTML = formatDate(currentTime);
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
-
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
-
-let iconElement = document.querySelector("#icon");
-iconElement.setAttribute ("src", 'http://openweathermap.org/img/wn/${response.data.weather[0].icon@2x.png');
 
 searchCity("Germantown");
