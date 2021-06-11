fdfe5dbded0b93e3c1b18a5627921fcafeba3ed6
Primabratinella <karen.pearl@hotmail.com>
2021-06-11 10:32:52 -0400

    Updated the displayweathercondition and celsius link

--- src/script.js
+++ src/script.js
@@ -38,8 +38,7 @@ function displayWeatherCondition(response) {
 
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-  axios.get(apiUrl).then(displayWeatherCondition);
-
+  
   iconElement.setAttribute (
     "alt", response.data.weather[0].main
   );
@@ -64,8 +63,9 @@ function handleSubmit(event) {
 function searchLocation(position) {
   let apiKey = "32560c27e8e23a3db17cfaff2b468cd2";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  
+  axios.get(apiUrl).then(displayWeatherCondition);
 }
+  
 
 function getCurrentLocation(event) {
   event.preventDefault();
@@ -111,7 +111,7 @@ let celsiusTemperature = null;
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
-let celsiusLink = document.querySelector("#celsisu-link");
+let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displaycelsiusTemperature);
 
 searchCity("Germantown");
