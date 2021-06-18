ce01c7193e95243f7e0a0859d6e93c908b0dbe66
Primabratinella <karen.pearl@hotmail.com>
2021-06-18 00:45:00 -0400

    Updated HTML, JS and CSS
    removed div script
    created new function of displayForecast and added div script from html
    updated css

--- src/script.js
+++ src/script.js
@@ -23,6 +23,27 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast () {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Fri", "Sat", "Sun", "Mon","Tues","Wed"];
+  
+  let forecastHTML= `<div class ="row">`;
+  days.forEach(function(day) {
+  forecastHTML = forecastHTML + `
+            <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img src="src/cloudy logo.png" alt="cloudy" width="45" />
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max"> 19° </span>
+                  <span class="weather-forecast-temperature-min"> 12° </span>
+                </div>
+            </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML= forecastHTML;
+}
+
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   let searchCity = "Germantown";
@@ -44,6 +65,8 @@ function displayWeatherCondition(response) {
   );
 
   celsiusTemperature = response.data.main.temp;
+
+
 }
 
 function searchCity(city) {
@@ -115,3 +138,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displaycelsiusTemperature);
 
 searchCity("Germantown");
+displayForecast();
